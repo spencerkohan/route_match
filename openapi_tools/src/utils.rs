4e4826ce2e2e73ac64573b1c11416298baae904d
Spencer Kohan <spencerkohan@gmail.com>
2025-02-17 00:19:11 +0100

    fix: swagger version number

--- openapi_tools/src/utils.rs
+++ openapi_tools/src/utils.rs
@@ -4,7 +4,7 @@ use serde_json;
 pub fn to_v2(spec: OpenAPI) -> v2::OpenAPI {
     let mut result = v2::OpenAPI::default();
 
-    result.swagger = "2.0.0".to_string();
+    result.swagger = "2.0".to_string();
     result.info = serde_json::from_str(&serde_json::to_string(&spec.info).unwrap()).unwrap();
     result.paths = serde_json::from_str(&serde_json::to_string(&spec.paths).unwrap()).unwrap();
     result.security =
